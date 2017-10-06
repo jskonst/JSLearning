eea385d18450c83461ef2e8e1548f470dc2e968a
Aleksandr Aladin <alexandr050786@gmail.com>
2017-10-06 12:56:46 +0400

    Solutions for lesson 4 tasks

--- lesson_4.js
+++ lesson_4.js
@@ -3,14 +3,21 @@
 // 1) Using recursion
 function fib(n) {
 	if (n >= 1) {
-		let bool = (n === 1 || n === 2)
+		let bool = (n === 1 || n === 2);
 		return bool ? 1 : fib(n - 1) + fib(n - 2);
 	}
 } 
 
 // 2) Using loop
 function fib(n) {
-
+	let x = 1,
+			y = 1;
+	for (let i = 3; i <= n; i++) {
+		let z = x + y;
+		x = y;
+		y = z;
+	}
+	return y;
 }
 
 
@@ -32,6 +39,6 @@ function makeArray() {
 	 Вложенная функция, показывающая модальное окно с помощью alert, 
    получает внутреннее свойство scope, в котором находится ссылка на 
    объект lexicalEnvironment окружения (самовызвающаяся функция) , 
-   котором она была создана.
+   в котором она была создана.
 
 */
